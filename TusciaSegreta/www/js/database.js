4799c83e40226c0d66e5f8cca458e2b43cea9b0f
Italo Gazzaneo <igazzaneo@gmail.com>
2019-07-08 15:46:53 +0200

    Update database.js

--- TusciaSegreta/www/js/database.js
+++ TusciaSegreta/www/js/database.js
@@ -45,7 +45,7 @@ function recuperaPassword() {
       async: false,
 
     }).complete(function(response) {
-      showMessage(response.responseText);
+      showMessage(response.responseText['msg']);  
     });
   } else if(username == "") {
     showMessage("Nome utente obbligatorio");
