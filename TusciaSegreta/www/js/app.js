966fbf1cc2254b2deaa804d2208f653daf6f680f
Italo Gazzaneo <igazzaneo@gmail.com>
2019-11-18 16:44:00 +0100

    Update app.js

--- TusciaSegreta/www/js/app.js
+++ TusciaSegreta/www/js/app.js
@@ -26,9 +26,26 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 document.addEventListener("backbutton", testBackButton, false);
 
+var lastTimeBackPress=0;
+var timePeriodToExit=2000;
 function testBackButton() {
     // Handle the back button
-    alert("BackButton");
+    e.preventDefault();
+    e.stopPropagation();
+    if(new Date().getTime() - lastTimeBackPress < timePeriodToExit){
+        navigator.app.exitApp();
+    } else {
+        window.plugins.toast.showWithOptions(
+            {
+              message: "Press again to exit.",
+              duration: "short", // which is 2000 ms. "long" is 4000. Or specify the nr of ms yourself.
+              position: "bottom",
+              addPixelsY: -40  // added a negative value to move it up a bit (default 0)
+            }
+          );
+
+        lastTimeBackPress=new Date().getTime();
+    }
 }
 
 document.addEventListener('prechange', function(event) {
