5dcc19ab54fd617d7d0a1e25f867bb39be514382
Italo Gazzaneo <igazzaneo@gmail.com>
2020-04-30 00:59:28 +0200

    Modifica scheda ESPLORA con navigazione

--- TusciaSegreta/www/js/map.js
+++ TusciaSegreta/www/js/map.js
@@ -14,3 +14,12 @@ function onMapError(error) {
     console.log('code: ' + error.code + '\n' +
         'message: ' + error.message + '\n');
 }
+
+function startNavigation(lat, long ) {
+  var sitoLatitudine = lat;
+  var sitoLongitudine = long;
+  var geocoords = sitoLatitudine+','+sitoLongitudine;
+  if(confirm("Vuoi avviare la navigazione?\n\nRicorda di aprire di nuovo l'app per seguire il percorso.")) {
+    window.open('google.navigation:q=' + geocoords + '&mode=d', '_system');
+  }
+}
