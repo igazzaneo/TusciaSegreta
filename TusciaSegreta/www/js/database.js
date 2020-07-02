129fa473200add3633dce52f2d71d76e5f0fe4e4
Italo Gazzaneo <igazzaneo@gmail.com>
2020-07-02 15:06:15 +0200

    Update database.js

--- TusciaSegreta/www/js/database.js
+++ TusciaSegreta/www/js/database.js
@@ -640,7 +640,7 @@ function getPercorsoSito(id, database, map, callback)
   }
 }
 
-/* funcione per prelevare i nodi da visualizzare dinamicamente sul percorso con multimedia o semplici info di percorso */
+/* funzione per prelevare i nodi da visualizzare dinamicamente sul percorso con multimedia o semplici info di percorso */
 /* modifica del 02/07/2020 */
 function getNodiPercorsoSito(id, database, map, callback)
 {
@@ -648,7 +648,7 @@ function getNodiPercorsoSito(id, database, map, callback)
     database.transaction(function(transaction) {
 
       //transaction.executeSql('SELECT nodo.id, nodo.latitudine, nodo.longitudine, nodo.percorso_id, nodo.descrizione, nodo.nome from nodo inner join percorso on percorso.id=nodo.percorso_id where percorso.sito_id=?', [id], function(transaction, resultSet) {
-      transaction.executeSql('SELECT nodo.id, nodo.latitudine, nodo.longitudine, nodo.percorso_id, nodo.descrizione, nodo.nome,	multimedia.descrizione, multimedia.tipo_multimedia_idfrom nodo inner join percorso on percorso.id=nodo.percorso_id left join nodo_ha_multimedia on nodo.id=nodo_ha_multimedia.nodo_idleft join multimedia on multimedia.id=nodo_ha_multimedia.multimedia_idwhere percorso.sito_id=82 and (multimedia.tipo_multimedia_id is null or multimedia.tipo_multimedia_id<>9)', [id], function(transaction, resultSet) {
+      transaction.executeSql('SELECT nodo.id, nodo.latitudine, nodo.longitudine, nodo.percorso_id, nodo.descrizione, nodo.nome,	multimedia.descrizione, multimedia.tipo_multimedia_id from nodo inner join percorso on percorso.id=nodo.percorso_id left join nodo_ha_multimedia on nodo.id=nodo_ha_multimedia.nodo_id left join multimedia on multimedia.id=nodo_ha_multimedia.multimedia_id where percorso.sito_id=? and (multimedia.tipo_multimedia_id is null or multimedia.tipo_multimedia_id<>9)', [id], function(transaction, resultSet) {
         var elenco = new Array();
 
         for(var x = 0; x < resultSet.rows.length; x++) {
@@ -678,7 +678,7 @@ function getNodiStaticiPercorsoSito(id, database, map, callback)
     database.transaction(function(transaction) {
 
       //transaction.executeSql('SELECT nodo.id, nodo.latitudine, nodo.longitudine, nodo.percorso_id, nodo.descrizione, nodo.nome from nodo inner join percorso on percorso.id=nodo.percorso_id where percorso.sito_id=?', [id], function(transaction, resultSet) {
-      transaction.executeSql('SELECT nodo.id, nodo.latitudine, nodo.longitudine, nodo.percorso_id, nodo.descrizione, nodo.nome,	multimedia.descrizione, multimedia.tipo_multimedia_idfrom nodo inner join percorso on percorso.id=nodo.percorso_id left join nodo_ha_multimedia on nodo.id=nodo_ha_multimedia.nodo_idleft join multimedia on multimedia.id=nodo_ha_multimedia.multimedia_idwhere percorso.sito_id=82 and multimedia.tipo_multimedia_id=9', [id], function(transaction, resultSet) {
+      transaction.executeSql('SELECT nodo.id, nodo.latitudine, nodo.longitudine, nodo.percorso_id, nodo.descrizione, nodo.nome,	multimedia.descrizione, multimedia.tipo_multimedia_id from nodo inner join percorso on percorso.id=nodo.percorso_id left join nodo_ha_multimedia on nodo.id=nodo_ha_multimedia.nodo_id left join multimedia on multimedia.id=nodo_ha_multimedia.multimedia_id where percorso.sito_id=? and multimedia.tipo_multimedia_id=9', [id], function(transaction, resultSet) {
         var elenco = new Array();
 
         for(var x = 0; x < resultSet.rows.length; x++) {
