b4be02ba2e680e04b0fc90c94a1fe671edea71a5
Italo Gazzaneo <igazzaneo@gmail.com>
2020-07-04 13:31:15 +0200

    Tolti messaggi di debug

--- TusciaSegreta/www/js/app.js
+++ TusciaSegreta/www/js/app.js
@@ -39,6 +39,8 @@ String.prototype.replaceAll = function(search, replacement) {
 
 //}
 
+document.addEventListener("deviceready", onResume, false);
+
 document.addEventListener("resume", onResume, false);
 function onResume() {
     //setTimeout(function() {
