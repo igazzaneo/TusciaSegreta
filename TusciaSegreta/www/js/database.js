055fea665361e5ac046db090dd3fe667d098efee
Italo Gazzaneo <igazzaneo@gmail.com>
2020-07-04 06:57:55 +0200

    Update database.js

--- TusciaSegreta/www/js/database.js
+++ TusciaSegreta/www/js/database.js
@@ -605,10 +605,12 @@ function setSitoInfo(sito) {
 
   $(".title").html(sito[1]);
   document.getElementById('video').src=sito[3].toString().replace(/watch\?v=/g, "embed/");
-  $(".content").html(sito[2]+"<br><br><br><br><br><br>");
-  var caratteristiche = "<div><div style='width: 30%'><i class='fas "+ sito[8][2] +"'></i><span>" + sito[8][0] + "</span></div>" +
+  $(".content").html(sito[2]+"<br><br>");
+
+  var caratteristiche = "<div class=\"divTable\"><div class=\"divTableRow\"><div class=\"divTableHead1\"><i class='fas "+ sito[8][2] +"'></i>"+ sito[8][0] +"</div><div class=\"divTableHead2\"><i class='fas "+ sito[9][2] +"'></i>" + sito[9][0] + " km</div><div class=\"divTableHead3\"><i class='fas "+ sito[10][2] +"'></i>" + sito[10][0] + " min</div></div></div>";
+  /*var caratteristiche = "<div><div style='width: 30%'><i class='fas "+ sito[8][2] +"'></i><span>" + sito[8][0] + "</span></div>" +
     "<div style='width: 30%'><i class='fas "+ sito[9][2] +"'></i><span>" + sito[9][0] + "</span></div>" +
-    "<div style='width: 30%'><i class='fas "+ sito[10][2] +"'></i><span>" + sito[10][0] + "</span></div></div>";
+    "<div style='width: 30%'><i class='fas "+ sito[10][2] +"'></i><span>" + sito[10][0] + "</span></div></div>";*/
   $(".caratteristiche").html(caratteristiche);
 }
 
